9900cfa047d6034bc99cf450d2c8da0c7011fdb8
lucasmoix <129341492+lucasmoix@users.noreply.github.com>
2024-02-24 18:59:36 -0300

    Creacion de proyecto

---
+++ Carrito-X/Carrito-X/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Carrito_X.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
